ba0cb1ada63993ae594a2b7a9a8b517b410a1577
Franco Ortega <francopdx@gmail.com>
2022-08-08 18:31:06 -0700

    create new bobble

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Setup
 
 1. `npx create-next-app@latest --example with-mongodb .`
+1. Add MongoURI string to env.local
+1. Connect to database in API handler
--- pages/api/bobbles.js
+++ pages/api/bobbles.js
@@ -1,3 +1,15 @@
+import clientPromise from '../../lib/mongodb';
+
 export default async function handler(req, res) {
-  res.json({ hello: 'world' });
+  const client = await clientPromise;
+
+  const db = client.db('demo-next-03-mongo');
+
+  // const data = await db.collection('bobbles').find({}).toArray();
+  // console.log(data);
+  const newBobble = req.query;
+  console.log(newBobble);
+  const response = await db.collection('bobbles').insertOne(newBobble);
+
+  res.json(response);
 }
