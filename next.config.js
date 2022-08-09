dec87cf91ea1b5e832f8fd6c76c0354184192da6
Franco Ortega <francopdx@gmail.com>
2022-08-09 08:39:27 -0700

    add API URL to env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://demo-next-03-mongo.vercel.app/bobbles'
-      }
-    ];
-  }
-};
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: 'https://demo-next-03-mongo.vercel.app/bobbles'
+//       }
+//     ];
+//   }
+// };
