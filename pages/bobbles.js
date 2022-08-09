2875398f2741ff957d86bcecbc8218132efd264f
Franco Ortega <francopdx@gmail.com>
2022-08-08 18:42:30 -0700

    add bobble with hard coded query params

--- pages/bobbles.js
+++ pages/bobbles.js
@@ -1,7 +1,19 @@
 const Bobbles = () => {
+  const onBobbleSubmit = async () => {
+    console.log('bobble added!!');
+
+    const data = await fetch(
+      'http://localhost:3000/api/bobbles?shape=round&color=red'
+    );
+
+    const response = await data.json();
+    console.log(response);
+  };
+
   return (
     <div>
       <h1>Bobbles</h1>
+      <button onClick={onBobbleSubmit}>Add Bobble</button>
     </div>
   );
 };
